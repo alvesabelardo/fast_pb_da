07c0da2fed11355e384429b00153d82b94522358
Abelardo <abelardoalves510@gmail.com>
2023-09-26 17:07:19 -0300

    Tabelas input adicionadas

--- PROJETO I/projeto/python_etl/etl.py
+++ PROJETO I/projeto/python_etl/etl.py
@@ -13,18 +13,40 @@ olist_cep = pd.read_csv('/docker-entrypoint-initdb.d/olist_customers_dataset.csv
 
 ## order_reviews = pd.DataFrame('./mongodb-init/init.js')
 
-tabela_produtos = olist_ordem_vendas.merge(olist_cep,
+#Informações de envio
+tabela_envio = olist_ordem_vendas.merge(olist_cep,
                             left_on='customer_id',
                             right_on='customer_id',
                             how='left')
+tabela_envio.rename(columns={'order_id': 'id_ordem', 'order_approved_at': 'data_aprovacao', 'order_estimated_delivery_date': 'estimativa_entrega',  'customer_id': 'fk_olist_cep', 'order_status': 'status_venda', 'customer_zip_code_prefix': 'cep', 'customer_city': 'cidade', 'customer_state': 'estado'}, inplace=True)
+tabela_envio = tabela_envio[['id_ordem', 'data_aprovacao','estimativa_entrega', 'status_venda', 'cep', 'cidade', 'estado', 'fk_olist_cep' ]]
+tabela_envio.data_aprovacao = pd.to_datetime(tabela_envio.data_aprovacao)
+tabela_envio.estimativa_entrega = pd.to_datetime(tabela_envio.estimativa_entrega)
+# print(tabela_envio.dtypes)
 
-tabela_produtos.rename(columns={'order_id': 'id', 'order_approved_at': 'data_aprovacao', 'order_estimated_delivery_date': 'estimativa_entrega',  'customer_id': 'fk_olist_cep', 'order_status': 'status_venda', 'customer_zip_code_prefix': 'cep', 'customer_city': 'cidade', 'customer_state': 'estado'}, inplace=True)
-tabela_produtos = tabela_produtos[['id', 'data_aprovacao','estimativa_entrega','fk_olist_cep', 'status_venda', 'cep', 'cidade', 'estado' ]]
-tabela_produtos.data_aprovacao = pd.to_datetime(tabela_produtos.data_aprovacao)
-tabela_produtos.estimativa_entrega = pd.to_datetime(tabela_produtos.estimativa_entrega)
+#Informações de produtos
+tabela_produtos = olist_produtos.merge(olist_ordem_items,
+                            left_on='product_id',
+                            right_on='product_id',
+                            how='left' )
+tabela_produtos['dimensoes'] = tabela_produtos.apply(lambda row: '{:.0f}x{:.0f}x{:.0f}'.format(row['product_width_cm'], row['product_height_cm'], row['product_length_cm']), axis=1)
+tabela_produtos = tabela_produtos.drop(['product_width_cm', 'product_height_cm', 'product_length_cm'], axis=1)
+tabela_produtos = tabela_produtos[['product_id', 'product_category_name', 'dimensoes', 'shipping_limit_date', 'price', 'freight_value', 'seller_id', 'order_id']]
+tabela_produtos.rename(columns={'product_id': 'id_produto', 'product_category_name': 'categoria', 'shipping_limit_date':'limite_data_envio', 'price':'valor_produto', 'freight_value':'valor_frete', 'seller_id':'id_venda', 'order_id':'fk_id_tabela_envio'}, inplace=True)
+tabela_produtos.limite_data_envio = pd.to_datetime(tabela_produtos.limite_data_envio)
 # print(tabela_produtos.dtypes)
 
-#Inserindo tabela produtos 
-tabela_produtos.to_sql(name='tabela_produtos', con=conn, if_exists='append', index=False, schema='ecommerce')
+#Informações de pagamentos
+tabela_pagamentos = olist_pagamentos.copy()
+tabela_pagamentos = tabela_pagamentos[['order_id', 'payment_type', 'payment_installments','payment_value']]
+tabela_pagamentos.rename(columns={ 'payment_type': 'tipo_pagamento', 'payment_installments': 'qtde_parcelas', 'payment_value':'valor' }, inplace=True)
+# print(tabela_pagamentos.dtypes)
+
+
+#Inserindo dados nas tabelas
+ 
+tabela_envio.to_sql(name='vendas', con=conn, if_exists='append', index=False, schema='ecommerce')
+tabela_produtos.to_sql(name='produtos', con=conn, if_exists='append', index=False, schema='ecommerce')
+tabela_pagamentos.to_sql(name='pagamentos', con=conn, if_exists='append', index=False, schema='ecommerce')
 
 conn.commit()
